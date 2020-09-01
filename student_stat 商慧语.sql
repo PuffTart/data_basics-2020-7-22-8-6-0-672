83b7aae175233f5ecb3e5f9f57678921bee370ba
PuffTart <45682067+PuffTart@users.noreply.github.com>
2020-09-01 14:25:39 +0800

    Add files via upload

---
+++ student_stat 商慧语.sql
@@ -0,0 +1,42 @@
+
+create table student_stat (
+-- TODO:
+)
+
+create procedure calc_student_stat
+-- TODO:
+
+select
+	t1.name
+	,t3.teacher
+	,t3.subject
+	,t5.avg_score
+	,t2.score
+	,t4.sum_score
+	,t2.score/t4.sum_score
+from
+	student t1
+left join
+	score t2
+	on t1.id = t2.student_id
+left join
+	subject t3  
+	on t2.subject_id = t3.id
+left join 
+	(select
+		student_id
+		,sum(score) sum_score
+	from
+		score a 
+	group by 
+		student_id) t4
+	on t2.student_id = t4.student_id
+left join 
+	(select
+		subject_id
+		,avg(score) avg_score
+	from
+		score a 
+	group by 
+		subject_id) t5
+	on t2.subject_id = t5.subject_id
